f8f79f13a800eb619069d78de4c9bde66f22bf58
sarkar-segfault <adhritsarkar@proton.me>
2026-01-08 18:08:12 +0530

    fixed pattern matching in main

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,17 @@ mod run;
 mod token;
 mod utils;
 
-use args::Command;
-
 fn main() {
     let info = args::parse();
     let text = &std::fs::read_to_string(&info.file)
         .unwrap_or_else(|e| fatal!("failed to open file: {}", e));
 
     match info.cmd {
-        Command::Token => println!("{:#?}", token::ize(&info.file, text)),
-        Command::Parse => println!(
+        args::Command::Token => println!("{:#?}", token::ize(&info.file, text)),
+        args::Command::Parse => println!(
             "{:#?}",
             ast::ify(&mut token::ize(&info.file, text), &info.file)
         ),
-        _ => run::start(ast::ify(&mut token::ize(&info.file, text), &info.file)),
+        args::Command::Start => run::start(ast::ify(&mut token::ize(&info.file, text), &info.file)),
     }
 }
