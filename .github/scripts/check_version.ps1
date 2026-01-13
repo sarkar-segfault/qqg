cdd94409b17f389e81b58a38bad0d3e581b5e332
sarkar-segfault <adhritsarkar@proton.me>
2026-01-13 20:18:17 +0530

    improved ci+cd... again...

--- .github/scripts/check_version.ps1
+++ .github/scripts/check_version.ps1
@@ -1,6 +1,5 @@
-$ErrorActionPreference = "Stop"
 $version = (cargo metadata --format-version 1 | ConvertFrom-Json).packages[0].version
 
 if ($version -ne (Invoke-RestMethod "https://crates.io/api/v1/crates/quick-quiz-generator").crate.newest_version) {
-    $version
+    Out-File -FilePath $env:GITHUB_OUTPUT -Append -InputObject "version=$version"
 }
