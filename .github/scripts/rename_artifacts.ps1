6ea7af6af6f83fc8482a5b74326150234e7e84a3
sarkar-segfault <adhritsarkar@proton.me>
2026-01-20 12:18:52 +0530

    changed cargo fmt to cargo fmt --check, and added versioning to release files

--- .github/scripts/rename_artifacts.ps1
+++ .github/scripts/rename_artifacts.ps1
@@ -1,8 +1,11 @@
+param(
+    [string]$version
+)
 $ErrorActionPreference = "Stop"
 
-Copy-Item "dist/windows-2025/qqg.exe" "dist/qqg-x86_64-windows.exe"
-Copy-Item "dist/windows-11-arm/qqg.exe" "dist/qqg-aarch64-windows.exe"
-Copy-Item "dist/ubuntu-24.04/qqg" "dist/qqg-x86_64-linux"
-Copy-Item "dist/ubuntu-24.04-arm/qqg" "dist/qqg-aarch64-linux"
-Copy-Item "dist/macos-15/qqg" "dist/qqg-aarch64-apple"
-Copy-Item "dist/macos-15-intel/qqg" "dist/qqg-x86_64-apple"
+Copy-Item "dist/windows-2025/qqg.exe" "dist/qqg-$version-x86_64-windows.exe"
+Copy-Item "dist/windows-11-arm/qqg.exe" "dist/qqg-$version-aarch64-windows.exe"
+Copy-Item "dist/ubuntu-24.04/qqg" "dist/qqg-$version-x86_64-linux"
+Copy-Item "dist/ubuntu-24.04-arm/qqg" "dist/qqg-$version-aarch64-linux"
+Copy-Item "dist/macos-15/qqg" "dist/qqg-$version-aarch64-apple"
+Copy-Item "dist/macos-15-intel/qqg" "dist/qqg-$version-x86_64-apple"
